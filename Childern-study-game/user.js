80760ce4e84346f077fdc1ed22b1c45c3b4316b9
Yaroslav Chaban <chabanyaroslav46@gmail.com>
2022-04-21 20:21:03 +0200

    Mögliche User Class

--- Childern-study-game/user.js
+++ Childern-study-game/user.js
@@ -1,1 +1,13 @@
-console.log()
+class User{
+    constructor(id,name){
+        this.id = id,
+        this.name = name,
+        // score for games
+        this.scoreByMemory = 0,
+        this.scoreByGS = 0      
+    }
+
+    getName(){
+        return this.name
+    }
+}
