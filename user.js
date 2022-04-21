38c60e019bd6015569f35ac31783d5f72cbcaed4
Yaroslav Chaban <chabanyaroslav46@gmail.com>
2022-04-21 20:40:25 +0200

    Mögliche User Class

---
+++ user.js
@@ -0,0 +1,21 @@
+class User{
+    constructor(id,name){
+        this.id = id,
+        this.name = name,
+        // score for games
+        this.scoreByMemory = 0,
+        this.scoreByGS = 0      
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getSoreByMemory(){
+        return this.getSoreByMemory
+    }
+
+    getScoreByGS(){
+        return this.scoreByGS
+    }
+}
