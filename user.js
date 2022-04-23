20b30dac51168ccce59148ff8642f1aea1a0a7b8
Yaroslav Chaban <chabanyaroslav46@gmail.com>
2022-04-23 15:45:21 +0200

    Using Bootstrap, create new folder for image

--- user.js
+++ user.js
@@ -1,7 +1,8 @@
 class User{
-    constructor(id,name){
+    constructor(id,name, image){
         this.id = id,
         this.name = name,
+        this.image = image, // url of image for user acount 
         // score for games
         this.scoreByMemory = 0,
         this.scoreByGS = 0      
