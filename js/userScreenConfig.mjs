e32c750fb8bce75ebc85d8adfb5e214b86afd2cf
Yaroslav Chaban <chabanyaroslav46@gmail.com>
2022-05-24 10:01:29 +0200

    Update

--- js/userScreenConfig.mjs
+++ js/userScreenConfig.mjs
@@ -7,7 +7,7 @@ const imageOfUser = document.getElementById("imageOfUser");
 imageOfUser.src = window.getImage();
 
 const scoreOfMemory = document.getElementById("scoreOfMemory");
-scoreOfMemory.innerText = "Memory: " + window.getScoreMemory() + " point";
+scoreOfMemory.innerText = "Memory: " + window.getScoreMemory() + " points";
 
 const scoreOfGS = document.getElementById("scoreOfGS");
-scoreOfGS.innerText = "Guess sounds: " + window.getScoreGS() + "point";
+scoreOfGS.innerText = "Guess sounds: " + window.getScoreGS() + " points";
